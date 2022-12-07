7c96d87995dd5771e0fd7385adfd0f7115e8211b
ptuan123987 <87867738+ptuan123987@users.noreply.github.com>
2022-12-07 22:42:12 +0700

    thêm tí def main :))

--- flapSanta.py
+++ flapSanta.py
@@ -28,6 +28,16 @@ def create_tube():
     return bot_tube, top_tube
 
 
+# vẽ ống
+def daw_tube(tubes):
+    for tube in tubes:
+        if tube.bottom >= 600:
+            screen.blit(tube_screen, tube)
+        else:
+            # lật ống theo chiều Oy
+            flip_tube = pygame.transform.flip(tube_screen, False, True)  
+            screen.blit(flip_tube, tube)
+            
 # di chuyển ống sang bên trái trả lại một list mới
 def move_tube(tubes):
     for tube in tubes:
@@ -35,14 +45,6 @@ def move_tube(tubes):
     visible_tubes = [tube for tube in tubes if tube.right > -50]
     return visible_tubes
 
-# vẽ ống
-def daw_tube(tubes):
-    for tube in tubes:
-        if tube.bottom >= 600:
-            screen.blit(tube_screen, tube)
-        else:
-            flip_tube = pygame.transform.flip(tube_screen, False, True)  # lật ống theo chiều Oy
-            screen.blit(flip_tube, tube)
 
 
 #  va chạm
@@ -95,17 +97,9 @@ def score_screen(state):
         high_score_rect = high_score_screen.get_rect(center=(216, 630))
         screen.blit(high_score_screen, high_score_rect)
 
-
-
-def update_score(score, high_score):
-    if score > high_score:
-        high_score = score
-    return high_score
-
-
-def tube_score():
+def player_score():
     global score, can_score
-
+  
     if tube_list:
         for tube in tube_list:
             if 95 < tube.centerx < 105 and can_score:
@@ -115,6 +109,14 @@ def tube_score():
 
             if tube.centerx < 0:
                 can_score = True
+
+def update_score(score, high_score):
+    if score > high_score:
+        high_score = score
+    return high_score
+
+
+
                 
 #nhấn p là pause               
 def pause(stop):
@@ -122,15 +124,16 @@ def pause(stop):
     while stop: 
         my_font = pygame.font.Font('Santa Claus Sounds/ComicSansMS3.ttf', 90)
         text_surface = my_font.render("Paused", True, GRAY)
-        rect = text_surface.get_rect(center=(200, 150))
+        rect = text_surface.get_rect(center=(216, 384))
         screen.blit(text_surface, rect)
 
         for event in pygame.event.get():
             pygame.display.update()
-            # unpause the game
+            # un pause the game
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE or event.key == pygame.K_p:
                     stop = False
+           
                     
 def play_countdown(countdown,active) :
     
@@ -162,93 +165,6 @@ def mode_day_night() :
         background = pygame.image.load('Santa Claus IMages/Light.PNG') 
     return background
 
-
-
-pygame.init()
-pygame.display.set_caption('Flapp Santa')
-image_icon = pygame.image.load('Santa Claus Images/santa_icon.PNG')
-pygame.display.set_icon(image_icon)
-screen = pygame.display.set_mode((432, 768))
-clock = pygame.time.Clock()
-game_font = pygame.font.Font('Santa Claus Sounds/04B_19.TTF', 35)
-
-# tạo các biến cho trò chơi
-gravity = 0.2
-santa_movement = 0
-active = True
-score = 0
-high_score = 0
-can_score = True
-stop =False
-WHITE = (255,255,255)
-GRAY = (150,150,150)
-day_night = False
-
-#dark mode
-day = False
-night = False
-start = True
-play = False
-countdown = False
-flags = False
-# chèn background
-# convert đổi file hình ảnh thành file nhẹ hơn để pygame load nhanh hơn
-background = pygame.image.load('Santa Claus Images/dark.PNG').convert()
-
-
-
-# chèn sàn
-floor = pygame.image.load('Santa Claus Images/Floor.png').convert()
-floor = pygame.transform.scale2x(floor)
-floor_x = 0
-
-# tạo santa
-
-santa_down = pygame.image.load('Santa Claus Images/santa_icon.PNG').convert_alpha()
-santa_mid = pygame.image.load('Santa Claus Images/santa_icon_1.PNG').convert_alpha()
-santa_up = pygame.image.load('Santa Claus Images/santa_icon.PNG').convert_alpha()
-
-santa_list = [santa_down, santa_mid, santa_up]
-santa_index = 0
-santa = santa_list[santa_index]
-santa_rect = santa.get_rect(center=(100, 384))
-
-# tạo timer cho santa
-santa_flap = pygame.USEREVENT + 1
-pygame.time.set_timer(santa_flap, 216)
-
-# tạo cột
-tube_screen = pygame.image.load('Santa Claus Images/tube.PNG').convert()
-tube_screen = pygame.transform.scale2x(tube_screen)
-tube_list = []
-
-# tạo timer
-spawn_tube = pygame.USEREVENT
-pygame.time.set_timer(spawn_tube, 1700)
-tube_height = [500, 300, 400]
-
-# tọa màn  hình kết thúc
-game_over_screen = pygame.image.load('Santa Claus Images/Untitled_Artwork 2.png').convert_alpha()
-game_over_rect = game_over_screen.get_rect(center=(216, 384))
-screen.blit(game_over_screen, game_over_rect)
-
-# chèn âm thanh
-flap_sound = pygame.mixer.Sound('Santa Claus Sounds/sound/sfx_wing.wav')
-flap_sound.set_volume(0.5)
-
-hit_sound = pygame.mixer.Sound('Santa Claus Sounds/sound/sfx_hit.wav')
-hit_sound.set_volume(0.5)
-
-score_sound = pygame.mixer.Sound('Santa Claus Sounds/sound/sfx_point.wav')
-score_sound.set_volume(0.5)
-
-score_sound_c = 100
-score_event = pygame.USEREVENT + 2
-pygame.time.set_timer(score_event, 100)
-
-
-
-
 def button(x2, y2, width, height, active_color, action=None):
     cur = pygame.mouse.get_pos()
     if x2 + width > cur[0] > x2 and y2 + height > cur[1] > y2:
@@ -262,84 +178,174 @@ def start_screen() :
     pass
 
 
-# while loop của trò chơi
-while True:
+def main() : 
 
-    # if start :
-    #     start_screen()
-    #     start = False
-    #     play = True
-    # elif play: 
-    for event in pygame.event.get():
-
-        # nhấn phím thoát khỏi game
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()  # thoát hệ thống
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and active:
-                santa_movement = 0
-                santa_movement -= 6
-                flap_sound.play()
-                
-            
-                    
-            if event.key == pygame.K_SPACE and active == False:
-                
-                active = True
-                countdown = True
-                play_countdown(countdown,active)
-                tube_list.clear()
-                santa_rect.center = (100, 384)
-                santa_movement = 0
-                score = 0
-            
-                
-            if event.key == pygame.K_p:
-                stop = True
-                pause(stop)
-                        
-        if event.type == spawn_tube:
-            tube_list.extend(create_tube())
-            
-        if event.type == santa_flap:
-            if santa_index < 2:
-                santa_index += 1
-            else:
-                santa_index = 0
-            santa, santa_rect = santa_animation()
-        
-            
-            
-            
-    screen.blit(background, (0, 0))  # thêm background vào của sổ game
-    if active:
-        # santa
-        santa_movement += gravity
-        turningd_santa = turning_santa(santa)
-        santa_rect.centery += santa_movement  # di chuyển xuống dưới
-        screen.blit(turningd_santa, santa_rect)
-        active = collision(tube_list)
-        # ống
-        tube_list = move_tube(tube_list)
-        daw_tube(tube_list)
-        tube_score()
-        score_screen('main game')
-
-    else:
-
-        screen.blit(game_over_screen, game_over_rect)
-        high_score = update_score(score, high_score)
-        score_screen('game over')
-        
-    # sàn
-    floor_x -= 1  # di chuyển lùi lại phía bên trái
-    draw_floor()  # vẽ ống lên màn hình
-
-    if floor_x <= -432:
-        floor_x = 0
-    screen.blit(floor, (floor_x, 650))
+    global image_icon,screen,clock,gravity,santa_movement, active,score,high_score,stop,WHITE,GRAY,can_score,day_night,day,night,start,play,countdown,background,floor,floor_x,santa_up,santa_down,santa_mid,santa_list,santa_index,santa,santa_rect,santa_flap,tube_screen,tube_list,spawn_tube,tube_height,game_over_screen,game_over_rect,flap_sound,hit_sound,score_sound,score_sound_c,score_event,game_font
     
-    pygame.display.flip()
-    clock.tick(90)
+    pygame.init()
+    pygame.display.set_caption('Flapp Santa')
+    image_icon = pygame.image.load('Santa Claus Images/santa_icon.PNG')
+    pygame.display.set_icon(image_icon)
+    screen = pygame.display.set_mode((432, 768))
+    clock = pygame.time.Clock()
+    game_font = pygame.font.Font('Santa Claus Sounds/04B_19.TTF', 35)
+
+    # tạo các biến cho trò chơi
+    gravity = 0.2
+    santa_movement = 0
+    active = True
+    score = 0
+    high_score = 0
+    can_score = True
+    stop =False
+    WHITE = (255,255,255)
+    GRAY = (150,150,150)
+    day_night = False
+
+    #dark mode
+    day = False
+    night = False
+    start = True
+    play = False
+    countdown = False
+    flags = False
+    # chèn background
+    # convert đổi file hình ảnh thành file nhẹ hơn để pygame load nhanh hơn
+    background = pygame.image.load('Santa Claus Images/dark.PNG').convert()
+
+
+
+    # chèn sàn
+    floor = pygame.image.load('Santa Claus Images/Floor.png').convert()
+    floor = pygame.transform.scale2x(floor)
+    floor_x = 0
+
+    # tạo santa
+
+    santa_down = pygame.image.load('Santa Claus Images/santa_icon.PNG').convert_alpha()
+    santa_mid = pygame.image.load('Santa Claus Images/santa_icon_1.PNG').convert_alpha()
+    santa_up = pygame.image.load('Santa Claus Images/santa_icon.PNG').convert_alpha()
+
+    santa_list = [santa_down, santa_mid, santa_up]
+    santa_index = 0
+    santa = santa_list[santa_index]
+    santa_rect = santa.get_rect(center=(100, 384))
+
+    # tạo timer cho santa
+    santa_flap = pygame.USEREVENT + 1
+    pygame.time.set_timer(santa_flap, 216)
+
+    # tạo cột
+    tube_screen = pygame.image.load('Santa Claus Images/tube.PNG').convert()
+    tube_screen = pygame.transform.scale2x(tube_screen)
+    tube_list = []
+
+    # tạo timer
+    spawn_tube = pygame.USEREVENT
+    pygame.time.set_timer(spawn_tube, 1700)
+    tube_height = [500, 300, 400]
+
+    # tọa màn  hình kết thúc
+    game_over_screen = pygame.image.load('Santa Claus Images/Untitled_Artwork 2.png').convert_alpha()
+    game_over_rect = game_over_screen.get_rect(center=(216, 384))
+    screen.blit(game_over_screen, game_over_rect)
+
+    # chèn âm thanh
+    flap_sound = pygame.mixer.Sound('Santa Claus Sounds/sound/sfx_wing.wav')
+    flap_sound.set_volume(0.5)
+
+    hit_sound = pygame.mixer.Sound('Santa Claus Sounds/sound/sfx_hit.wav')
+    hit_sound.set_volume(0.5)
+
+    score_sound = pygame.mixer.Sound('Santa Claus Sounds/sound/sfx_point.wav')
+    score_sound.set_volume(0.5)
+
+    score_sound_c = 100
+    score_event = pygame.USEREVENT + 2
+    pygame.time.set_timer(score_event, 100)
+
+
+    # while loop của trò chơi
+    while True:
+
+        # if start :
+        #     start_screen()
+        #     start = False
+        #     play = True
+        # elif play: 
+        for event in pygame.event.get():
+
+            # nhấn phím thoát khỏi game
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()  # thoát hệ thống
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and active:
+                    santa_movement = 0
+                    santa_movement -= 6
+                    flap_sound.play()
+                    
+                
+                        
+                if event.key == pygame.K_SPACE and active == False:
+                    
+                    active = True
+                    countdown = True
+                    play_countdown(countdown,active)
+                    tube_list.clear()
+                    santa_rect.center = (100, 384)
+                    santa_movement = 0
+                    score = 0
+                
+                    
+                if event.key == pygame.K_p:
+                    stop = True
+                    pause(stop)
+                            
+            if event.type == spawn_tube:
+                tube_list.extend(create_tube())
+                
+            if event.type == santa_flap:
+                if santa_index < 2:
+                    santa_index += 1
+                else:
+                    santa_index = 0
+                santa, santa_rect = santa_animation()
+            
+                
+                
+                
+        screen.blit(background, (0, 0))  # thêm background vào của sổ game
+        if active:
+            # santa
+            santa_movement += gravity
+            turningd_santa = turning_santa(santa)
+            santa_rect.centery += santa_movement  # di chuyển xuống dưới
+            screen.blit(turningd_santa, santa_rect)
+            active = collision(tube_list)
+            # ống
+            tube_list = move_tube(tube_list)
+            daw_tube(tube_list)
+            player_score()
+            score_screen('main game')
+
+        else:
+
+            screen.blit(game_over_screen, game_over_rect)
+            high_score = update_score(score, high_score)
+            score_screen('game over')
+            
+        # sàn
+        floor_x -= 1  # di chuyển lùi lại phía bên trái
+        draw_floor()  # vẽ ống lên màn hình
+
+        if floor_x <= -432:
+            floor_x = 0
+        screen.blit(floor, (floor_x, 650))
+        
+        pygame.display.flip()
+        clock.tick(90)
+
+if __name__ == '__main__':
+    main()
